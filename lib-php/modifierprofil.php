48ad9f8e1db29780fdd9cf535c99a439dd0b390e
Andry ANDRIANAIVO <andryhsm@gmail.com>
2017-03-28 17:41:35 +0200

    menu patient

--- lib-php/modifierprofil.php
+++ lib-php/modifierprofil.php
@@ -107,7 +107,6 @@ $data = $req->fetch();
                             <nav class="site-nav" role="navigation" style="">
                                 <ul id="menu-main" class="main-nav">
                                     <li id="menu-item-71" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="../carte.php"><span class="menu-item-label">Carte</span></a></li>
-                                    <li id="menu-item-71" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="#"><span class="menu-item-label">Notification</span></a></li>
                                     <li id="menu-item-71" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="./modifierprofil.php"><span class="menu-item-label">Modifier mon profil</span></a></li>
                                     <li id="menu-item-71" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="../contact1.html"><span class="menu-item-label">Contact</span></a></li>
                                     <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="./deconnexion.php"><span class="menu-item-label">Deconnexion</span></a></li>
@@ -131,7 +130,6 @@ $data = $req->fetch();
                             <nav class="site-nav" role="navigation" style="">
                                 <ul id="menu-main1" class="main-nav">
                                     <li id="menu-item-71" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="../carte.php"><span class="menu-item-label">Carte</span></a></li>
-                                    <li id="menu-item-71" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="#"><span class="menu-item-label">Notification</span></a></li>
                                     <li id="menu-item-71" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="./modifierprofil.php"><span class="menu-item-label">Modifier mon profil</span></a></li>
                                     <li id="menu-item-71" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="../contact1.html"><span class="menu-item-label">Contact</span></a></li>
                                     <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="./deconnexion.php"><span class="menu-item-label">Deconnexion</span></a></li>
